1f980691c781d5c5576f47aaea449dcc0b0b8e08
codewizard870 <alenzer0902@gmail.com>
2024-06-21 13:24:21 -0400

    fade effect

--- src/components/Features/Home/ActionFade/index.tsx
+++ src/components/Features/Home/ActionFade/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import './index.css';
+import "./index.css";
+
+import React, { useEffect, useState } from "react";
 
 interface Props {
   imgUrl: string;
@@ -7,47 +8,10 @@ interface Props {
 }
 
 const ActionFade: React.FC<Props> = ({ imgUrl, replaceImgUrl }) => {
-  const [fadeProp, setFadeProp] = useState({
-    fade: 'fade-in',
-  });
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsVisible(prevStatus => !prevStatus);
-    }, 4000);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const timeout = setInterval(() => {
-      if (fadeProp.fade === 'fade-in') {
-        setFadeProp({
-          fade: 'fade-out'
-        })
-      } else {
-        setFadeProp({
-          fade: 'fade-in'
-        })
-      }
-    }, 2000);
-
-    return () => clearInterval(timeout)
-  }, [fadeProp])
-
   return (
-    <div className="relative flex items-center justify-center">
-
-      {isVisible ? (<img src={imgUrl}
-        alt=""
-        className={`${fadeProp.fade} w-[75%] md:w-full lg:w-full`}
-      />) :
-        (
-          <img src={replaceImgUrl}
-            alt=""
-            className={`${fadeProp.fade} w-[75%] md:w-full lg:w-full`}
-          />
-        )}
+    <div className="relative flex items-center justify-center w-[75%] h-[50px]">
+      <img src={imgUrl} alt="" className={`a-fade absolute`} />
+      <img src={replaceImgUrl} alt="" className={`b-fade absolute`} />
     </div>
   );
 };
