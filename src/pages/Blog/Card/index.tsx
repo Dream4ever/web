80048510a6a8679e3d49a28b2f542117d64733cb
codewizard870 <alenzer0902@gmail.com>
2024-08-13 13:34:50 -0300

    fixed some issues

--- src/pages/Blog/Card/index.tsx
+++ src/pages/Blog/Card/index.tsx
@@ -5,23 +5,16 @@ interface Props {
   data: CardType
 }
 const Card: React.FC<Props> = ({ data }) => {
-  const contentRef = useRef<any>(null);
   const navigate = useNavigate();
-  
-  const toggleMouseEnter = () => {
-    (contentRef.current as any).style.color = "#9264F8";
-  }
-  const toggleMouseOut = () => {
-    (contentRef.current as any).style.color = "#FFFFFF";
-  }
+
 
   return (
-    <div className={`flex flex-col w-full md:w-[383px] h-[477px] p-4 border rounded-xl border-[#5D5D5D] hover:bg-[#1B1B1B] hover:cursor-pointer gap-4`}
-      onMouseOver={toggleMouseEnter} onMouseOut={toggleMouseOut} onClick={() => {navigate('/blog/detail')}}>
+    <div className={`flex flex-col w-full md:w-[383px] h-[477px] p-4 border rounded-xl border-[#5D5D5D] group hover:bg-[#1B1B1B] hover:cursor-pointer gap-4`}
+      onClick={() => { navigate('/blog/detail') }}>
       <img src={data.image} className="w-[351px]" alt="" />
       <div className="flex flex-col items-start justify-between p-2 ">
         <button className="h-7 px-[10px] py-1 bg-primary-default text-sm font-Outfit font-medium leading-[20px] mb-3 rounded-md">{data.label}</button>
-        <p ref={contentRef} className={` text-start text-[22px] h-[93px] font-medium leading-[140%] font-Outfit mb-4`}>
+        <p className={` text-start text-[22px] h-[93px] font-medium leading-[140%] group-hover:text-[#9264F8] font-Outfit mb-4`}>
           {data.content}
         </p>
         <p className="text-base font-normal leading-[150%] text-[#999]">{data.date}</p>
